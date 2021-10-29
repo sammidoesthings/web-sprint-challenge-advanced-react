f2946fb55aa04744f83a6ee2d99fc0dafce19408
Samantha <samanthaltl.sfh@gmail.com>
2021-10-29 13:32:09 -0700

    done

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,41 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+    render(<CheckoutForm/>)
+});
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", async () => {
+    render(<CheckoutForm/>)
+
+    const firstName = screen.getByLabelText(/First Name/i)
+    userEvent.type(firstName, 'Samantha');
+
+    const lastName = screen.getByLabelText(/Last Name/i)
+    userEvent.type(lastName, 'Little');
+
+    const address = screen.getByLabelText(/Address/i)
+    userEvent.type(address, '454 Plant Place');
+
+    const city = screen.getByLabelText(/City/i)
+    userEvent.type(city, 'San Francisco');
+
+    const state = screen.getByLabelText(/State/i)
+    userEvent.type(state, 'CA');
+
+    const zip = screen.getByLabelText(/zip/i)
+    userEvent.type(zip, '94151');
+
+    const button = screen.getByRole('button')
+    userEvent.click(button);
+
+    await waitFor(() => {
+        const successMessage = screen.queryByTestId('successMessage')
+        expect(successMessage).toBeInTheDocument();
+    })
+});
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,30 @@
 // write your custom hook here to control your checkout form
+
+import React, { useState } from 'react';
+
+const initialValue = {
+    firstName: '',
+    lastName: '',
+    address: '',
+    city: '',
+    state: '',
+    zip: ''
+};
+
+export const useForm = () => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initialValue);
+    
+    const handleChanges = (event) => {
+        setValues({ ...values, [event.target.name]: event.target.value });
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setShowSuccessMessage(true);
+    }
+
+    return [showSuccessMessage, handleChanges, handleSubmit, values]
+}
+
+export default useForm
